ebb75feaa0cf7fcff4f5179a0d412511abbe462e
williamwhang <whang.william@gmail.com>
2022-01-13 21:45:28 -0300

    Criando o Footer

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,11 +3,13 @@ import * as S from './styles';
 
 //NOSSOS COMPONENTES
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home() {
     return (
         <S.Container>
             <Header />
+            <Footer />
         </S.Container>
     )
 }
