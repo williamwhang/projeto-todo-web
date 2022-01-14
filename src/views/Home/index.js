1189fa1fddaa40acf14847ca3acc7751541019e8
williamwhang <whang.william@gmail.com>
2022-01-14 08:45:22 -0300

    Criado os Filtros de tarefas

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 
 //NOSSOS COMPONENTES
 import Header from '../../components/Header';
+import FilterCard from '../../components/FilterCard';
 import Footer from '../../components/Footer';
 
 function Home() {
+    const [filterActived, setFilterActived] = useState('today');
     return (
         <S.Container>
             <Header />
+
+            <S.FilterArea>
+                <button type="button" onClick={() => setFilterActived("all")}>
+                    <FilterCard title="Todos" actived={filterActived == 'all'} />
+                </button>
+
+                <button type="button" onClick={() => setFilterActived("today")}>
+                    <FilterCard title="Hoje" actived={filterActived == 'today'} />
+                </button>
+
+                <button type="button" onClick={() => setFilterActived("week")}>
+                    <FilterCard title="Semana" actived={filterActived == 'week'} />
+                </button>
+
+                <button type="button" onClick={() => setFilterActived("month")}>
+                    <FilterCard title="Mês" actived={filterActived == 'month'} />
+                </button>
+                <button type="button" onClick={() => setFilterActived("year")}>
+                    <FilterCard title="Ano" actived={filterActived == 'year'} />
+                </button>
+
+            </S.FilterArea>
+
             <Footer />
         </S.Container>
     )
