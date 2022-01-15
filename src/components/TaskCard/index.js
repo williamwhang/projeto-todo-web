a93b62cdf3623e71fc4a5d381aa692487e43092e
williamwhang <whang.william@gmail.com>
2022-01-15 08:07:37 -0300

    Conectando o Frontend com o Backend e Listando as tarefas

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { format } from 'date-fns';
 import * as S from './styles';
 
 import iconDefault from '../../assets/default.png';
 
-function TaskCard() {
+function TaskCard({type, title, when }) {
+    const date = useMemo(() => format(new Date(when), 'dd/MM/yyyy') );
+    const hour = useMemo(() => format(new Date(when), 'HH:mm') );
+
     return (
         <S.Container>
             <S.TopCard>
                 <img src={iconDefault} alt="Icone da Tarefa"/>
-                <h3>Título da Tarefa</h3>
+                <h3>{title}</h3>
             </S.TopCard>
             <S.BottomCard>
-                <strong>14/01/2022</strong>
-                <span>11:00</span>
+                <strong>{date}</strong>
+                <span>{hour}</span>
             </S.BottomCard>
         </S.Container>
     )
