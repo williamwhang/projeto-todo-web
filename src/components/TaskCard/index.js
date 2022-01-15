1f04c276913c27447e094ba06d5eb38b59eff0db
williamwhang <whang.william@gmail.com>
2022-01-15 09:05:19 -0300

    Deixando os icones das tarefas dinâmicos

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -2,16 +2,16 @@ import React, { useMemo } from 'react';
 import { format } from 'date-fns';
 import * as S from './styles';
 
-import iconDefault from '../../assets/default.png';
+import typeIcons from '../../utils/typeIcons';
 
-function TaskCard({type, title, when }) {
-    const date = useMemo(() => format(new Date(when), 'dd/MM/yyyy') );
-    const hour = useMemo(() => format(new Date(when), 'HH:mm') );
+function TaskCard({ type, title, when }) {
+    const date = useMemo(() => format(new Date(when), 'dd/MM/yyyy'));
+    const hour = useMemo(() => format(new Date(when), 'HH:mm'));
 
     return (
         <S.Container>
             <S.TopCard>
-                <img src={iconDefault} alt="Icone da Tarefa"/>
+                <img src={typeIcons[type]} alt="Icone da Tarefa" />
                 <h3>{title}</h3>
             </S.TopCard>
             <S.BottomCard>
